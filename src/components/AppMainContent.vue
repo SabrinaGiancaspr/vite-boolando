8fc0d62cd3bf85eaeeaf7a189552420ae3d255b8
sabri <sabrinatwitter6@gmail.com>
2023-11-07 18:31:14 +0100

    add heart

--- src/components/AppMainContent.vue
+++ src/components/AppMainContent.vue
@@ -4,51 +4,36 @@ export default {
 
     data() {
         return {
-            
+
         };
     },
     props: {
         card: Object
     },
     methods: {
-        calcDiscount(price, discount) {
-            const priceNumber = parseFloat(price);
-            const discountNumber = parseFloat(discount);
-            if (discountNumber === 0) {
-                return priceNumber;
-            }
-            const totalDiscount = (priceNumber * discountNumber) / 100;
-            const finalPrice = priceNumber - totalDiscount;
-
-            return finalPrice.toFixed(2);
-        },
-
+        heartFunction(){
+            this.card.isInFavorites = !this.card.isInFavorites;
+        }
     },
 
 };
 </script>
 
 <template>
-    
-    <span class="heart"> &hearts;</span>
+    <span class="heart" @click="heartFunction()" :class="card.isInFavorites === true ? 'red-heart' : ' ' "> &hearts;</span>
     <img class="first-img" :src="card.frontImage" alt="" />
     <img class="img-none" :src="card.backImage" alt="" />
     <div class="tag-container">
-        <span :class="badge.type" v-for="badge in card.badges" >{{ badge.value }}</span> 
+        <span :class="badge.type" v-for="badge in card.badges">{{ badge.value }}</span>
     </div>
 
     <p class="brand">{{ card.brand }}</p>
-    <p class="description">{{ card.name.toUpperCase()}}</p>
+    <p class="description">{{ card.name.toUpperCase() }}</p>
     <div class="price-container">
-        <p class="discount-price">
-            {{ calcDiscount(card.price, card.discount) }} &euro;
-        </p>
         <p class="price" v-show="card.discount != 0">
             {{ card.price }} &euro;
         </p>
     </div>
-
-
 </template>
 
 <style lang="scss" scoped>
@@ -58,66 +43,66 @@ export default {
 
 
 
-    .heart {
-        position: absolute;
-        top: 10px;
-        right: 10px;
-        font-size: 30px;
-        background-color: white;
-        width: 40px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        aspect-ratio: 1;
+.heart {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    font-size: 30px;
+    background-color: white;
+    width: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    aspect-ratio: 1;
+}
 
-        &:hover {
-            color: red;
-        }
+.red-heart{
+    color: red;
+}
+
+.img-none {
+    display: none;
+}
+
+.tag-container {
+    display: flex;
+    gap: 10px;
+    position: absolute;
+    bottom: 100px;
+    color: white;
+
+    .discount {
+        order: -1;
+        background-color: red;
+        padding: 5px;
     }
 
-    .img-none {
-        display: none;
+    .tag {
+        background-color: green;
+        padding: 5px;
     }
 
-    .tag-container {
-        display: flex;
-        gap: 10px;
-        position: absolute;
-        bottom: 100px;
-        color: white;
+    .prova {
+        background-color: blue;
+        padding: 5px;
+    }
+}
 
-        .discount {
-            order: -1;
-            background-color: red;
-            padding: 5px;
-        }
+.description {
+    font-weight: 600;
+}
 
-        .tag {
-            background-color: green;
-            padding: 5px;
-        }
+.price-container {
+    display: flex;
+    gap: 10px;
 
-        .prova {
-            background-color: blue;
-            padding: 5px;
-        }
+    .price {
+        text-decoration: line-through;
     }
 
-    .description {
-        font-weight: 600;
+    .discount-price {
+        color: red;
+        font-weight: bold;
     }
-
-    .price-container {
-        display: flex;
-        gap: 10px;
-
-        .price {
-            text-decoration: line-through;
-        }
-
-        .discount-price {
-            color: red;
-            font-weight: bold;
-        }
-    }
+}
 </style>
